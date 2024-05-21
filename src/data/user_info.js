8db8058d70420909515d8b3a14203ddc11638c65
LuckyLLemos <luckyllemos@gmail.com>
2024-05-21 08:28:35 +0800

    Update userinfo photo url

--- src/data/user_info.js
+++ src/data/user_info.js
@@ -3,7 +3,7 @@ const info = {
         name: "Lucky Llemos",
         description: "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
         role: "Software Engineer",
-        photo: "../photo.jpg",
+        photo: "/photo.jpg",
         email: "luckyllemos0909@gmail.com",
         tracking_id: "G-BNTTBGHXSC"
     },
